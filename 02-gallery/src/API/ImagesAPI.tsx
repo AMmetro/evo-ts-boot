39b976d490df16c7ae6190ba6cc777241c9c55d0
AMmetro <metro.trend.now@gmail.com>
2021-05-03 22:11:10 +0300

    fixed env file

--- 02-gallery/src/API/ImagesAPI.tsx
+++ 02-gallery/src/API/ImagesAPI.tsx
@@ -1,10 +1,16 @@
 
-const id="Hb2DDWjcgFqjPs5ZFnN5yibl5YDqOPZvwEF_A6_YsQU"
+// const id="Hb2DDWjcgFqjPs5ZFnN5yibl5YDqOPZvwEF_A6_YsQU"
 
-  const baseURL= `https://api.unsplash.com/search/photos?client_id=${id}&per_page=20`
+//   id is stored inside ".env" file in root  directory
+//   process.env  - is object witch stored properties: "REACT_APP"
+//   once restart (yarn start) it make virtual object in RUNTIME
+//   SEE console.log(process.env)
 
-export const imagesAPI = {
+  const baseURL= `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_UNSPLASH_CLIENT_ID}&per_page=20`
+
+  export const imagesAPI = {
     getPhotos(searchTxt:string) {
+
        const promise =  fetch(`${baseURL}&query=${searchTxt}}`)
            .then((res) => {return res.json() })
            .then((data) => { return data.results})
